8e42925eb3a71c552ce61925f1833ee12d0776a1
sacha <gorse.sacha@gmail.com>
2022-10-31 14:05:44 +0100

    Ajout d'une page de login : non fonctionnelle pour l'instant

--- src/views/App.vue
+++ src/views/App.vue
@@ -20,9 +20,63 @@ export default {
 </script>
 
 <template>
-    <h1 class="text-6xl font-normal leading-normal mt-0 mb-2 text-rose-800">Hello Guys !</h1>
-    <button @click="count++">Count is: {{ count }}</button>
-    <p v-if="texte">{{ texte }}</p>
+    <section class="h-screen">
+  <div class="container px-6 py-12 h-full">
+    <div class="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
+      <div class="md:w-8/12 lg:w-6/12 mb-12 md:mb-0">
+        <img
+          src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
+          class="w-full"
+          alt="Phone image"
+        />
+      </div>
+      <div class="md:w-8/12 lg:w-5/12 lg:ml-20 text-center">
+        <h1 class="text-6xl font-bold leading-normal mb-10 text-blue-600">MyAgenda</h1>
+        <form>
+          <!-- Email input -->
+          <div class="mb-6">
+            <input
+              type="text"
+              class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white 
+                bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 
+                focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              placeholder="Nom d'utilisateur"
+            />
+          </div>
+
+          <!-- Password input -->
+          <div class="mb-6">
+            <input
+              type="password"
+              class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 
+                bg-white bg-clip-padding border border-solid border-gray-300 rounded transition 
+                ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              placeholder="Mot de passe"
+            />
+          </div>
+
+          <!-- Submit button -->
+          <button
+            type="submit"
+            class="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug 
+                uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg 
+                focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
+            data-mdb-ripple="true"
+            data-mdb-ripple-color="light"
+          >
+            Connexion
+          </button>
+
+          <div
+            class="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 
+            before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5"
+          >
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</section>
 </template>
 
 <style scoped>
