141f9e41230db015465354c31f906ee7fd520dd2
sacha <gorse.sacha@gmail.com>
2022-12-17 15:40:23 +0100

    modif

--- src/views/components/DayView.vue
+++ src/views/components/DayView.vue
@@ -1,3 +1,10 @@
 <template>
-    <div>This is DayView</div>
+    <div class="drop-shadow bg-[#f9fafb] w-[80%] m-auto rounded h-[50rem]"></div>
+    <div class="w-full p-4"></div>
 </template>
+
+<script>
+</script>
+
+<style scoped>
+</style>
