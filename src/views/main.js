cc8d58dab82373f719002a6868876e96a80ff6bf
sacha <gorse.sacha@gmail.com>
2022-12-18 15:15:27 +0100

    Ajout d'un plugin

--- src/views/main.js
+++ src/views/main.js
@@ -4,12 +4,14 @@ import App from './App.vue';
 import axios from 'axios';
 import router from './routes.js';
 import dayjs from "dayjs";
+import weekday from "dayjs/plugin/weekday";
 
 import './index.css';
 
 const app = createApp(App);
 
 // Permet d'avoir un instance unique de axios pour pouvoir faire des requetes
+dayjs.extend(weekday);
 app.config.globalProperties.$http = axios;
 app.config.globalProperties.$dayjs = dayjs;
 
